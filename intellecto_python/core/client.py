01fa78acbf6215f8a387d5d0e46e53d7c0dda36e
Kabir Asani <iamkabirasani@gmail.com>
2024-02-13 16:10:03 +0530

    Create core.client.py

---
+++ intellecto_python/core/client.py
@@ -0,0 +1,18 @@
+class IntellectoClient:
+    token: str
+
+    def __init__(
+        self,
+        token: str
+    ) -> None:
+        self.token = token
+
+
+class AsyncIntellectoClient:
+    token: str
+
+    def __init__(
+        self,
+        token: str
+    ) -> None:
+        self.token = token
