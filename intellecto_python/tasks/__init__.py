333910a8da695f282a0198a4b79ed0984139a108
Kabir Asani <iamkabirasani@gmail.com>
2024-02-15 18:38:03 +0530

    Update tasks/__init__.py

--- intellecto_python/tasks/__init__.py
+++ intellecto_python/tasks/__init__.py
@@ -1,4 +1,4 @@
-from .base import IntellectoModel, IntellectoBase, AsyncIntellectoBase
-from .nlp import IntellectoNLP, AsyncIntellectoNLP
-from .audio import IntellectoAudio, AsyncIntellectoAudio
-from .vision import IntellectoVision, AsyncIntellectoVision
+from .base import *
+from .nlp import *
+from .audio import *
+from .vision import *
