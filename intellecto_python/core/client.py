11e62075ad6facbe3bb0194403288f2eefa64cd0
Kabir Asani <iamkabirasani@gmail.com>
2024-02-15 15:28:19 +0530

    Update client.py

--- intellecto_python/core/client.py
+++ intellecto_python/core/client.py
@@ -1,18 +1,27 @@
-class IntellectoClient:
-    token: str
+from httpx import Client, AsyncClient
 
+
+class IntellectoClient(Client):
     def __init__(
         self,
         token: str
     ) -> None:
-        self.token = token
+        super().__init__(
+            base_url="https://api-inference.huggingface.co/models",
+            headers={
+                'Authorization': f'Bearer {token}'
+            }
+        )
 
 
-class AsyncIntellectoClient:
-    token: str
-
+class AsyncIntellectoClient(AsyncClient):
     def __init__(
         self,
         token: str
     ) -> None:
-        self.token = token
+        super().__init__(
+            base_url="https://api-inference.huggingface.co/models",
+            headers={
+                'Authorization': f'Bearer {token}'
+            }
+        )
