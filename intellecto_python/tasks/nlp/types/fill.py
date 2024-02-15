15ed0ec1b4030296f77b6c120057ef80d998acb1
Kabir Asani <iamkabirasani@gmail.com>
2024-02-15 18:38:16 +0530

    Update fill.py

--- intellecto_python/tasks/nlp/types/fill.py
+++ intellecto_python/tasks/nlp/types/fill.py
@@ -1,5 +1,25 @@
+from dataclasses import dataclass
+
 from ...base import IntellectoBase
 
 
+@dataclass
 class NLPFillModel(IntellectoBase):
-    pass
+    sequence: str
+    score: float
+    token: int
+    token_str: str
+
+    @staticmethod
+    def from_json(json: Any) -> 'NLPFillModel':
+        sequence = str(json.get('sequence'))
+        score = str(json.get('score'))
+        token = int(obj.get('token'))
+        token_str = str(obj.get('token_str'))
+
+        return NLPFillModel(
+            score=score,
+            sequence=sequence,
+            token=token,
+            token_str=token_str
+        )
