9a720d37ea913e20da679a15e7d133d1f23e6d8a
Kabir Asani <iamkabirasani@gmail.com>
2024-02-15 18:38:10 +0530

    Update base.py

--- intellecto_python/tasks/base.py
+++ intellecto_python/tasks/base.py
@@ -6,7 +6,7 @@ class IntellectoModel:
 
 
 class IntellectoBase:
-    client: IntellectoClient
+    token: str
 
     def __init__(
         self,
@@ -14,14 +14,36 @@ class IntellectoBase:
     ) -> None:
         """Construct a new synchronous intellecto base instance.
         """
+        self.token = token
 
-        self.client = IntellectoClient(
-            token=token
-        )
+    @property
+    def client(self) -> IntellectoClient:
+        with IntellectoClient(
+            base_url="https://api-inference.huggingface.co/models",
+            token=self.token
+        ) as client:
+            return client
+
+    def raise_error_based_on(status_code: int):
+        match status_code:
+            case 400:
+                raise IntellectoBadRequestError()
+            case 401:
+                raise IntellectoAuthenticationError()
+            case 403:
+                raise IntellectoPermissionDeniedError()
+            case 404:
+                raise IntellectoNotFoundError()
+            case 422:
+                raise IntellectoUnprocessableEntityError()
+            case 429:
+                raise IntellectoRateLimitError()
+            case _:
+                raise IntellectoInternalServerError()
 
 
 class AsyncIntellectoBase:
-    client: AsyncIntellectoClient
+    token: str
 
     def __init__(
         self,
@@ -29,7 +51,101 @@ class AsyncIntellectoBase:
     ) -> None:
         """Construct a new asynchronous intellecto base instance.
         """
+        self.token = token
 
-        self.client = AsyncIntellectoClient(
-            token=token
-        )
+    @property
+    def client(self) -> IntellectoClient:
+        with IntellectoClient(
+            base_url="https://api-inference.huggingface.co/models",
+            token=self.token
+        ) as client:
+            return client
+
+    def raise_error_based_on(status_code: int):
+        match status_code:
+            case 400:
+                raise IntellectoBadRequestError()
+            case 401:
+                raise IntellectoAuthenticationError()
+            case 403:
+                raise IntellectoPermissionDeniedError()
+            case 404:
+                raise IntellectoNotFoundError()
+            case 422:
+                raise IntellectoUnprocessableEntityError()
+            case 429:
+                raise IntellectoRateLimitError()
+            case _:
+                raise IntellectoInternalServerError()
+
+
+class IntellectoError:
+    pass
+
+
+class IntellectoAPIConnectionError(IntellectoError):
+    pass
+
+
+class IntellectoUnknownError(IntellectoError):
+    pass
+
+
+class IntellectoAPIStatusError(IntellectoError):
+    status_code: int
+
+    def __init__(
+        self,
+        status_code: int
+    ) -> None:
+        self.status_code = status_code
+
+
+class IntellectoBadRequestError(IntellectoAPIStatusError):
+    def __init__(
+        self
+    ) -> None:
+        super().__init__(status_code=400)
+
+
+class IntellectoAuthenticationError(IntellectoAPIStatusError):
+    def __init__(
+        self
+    ) -> None:
+        super().__init__(status_code=401)
+
+
+class IntellectoPermissionDeniedError(IntellectoAPIStatusError):
+    def __init__(
+        self
+    ) -> None:
+        super().__init__(status_code=403)
+
+
+class IntellectoNotFoundError(IntellectoAPIStatusError):
+    def __init__(
+        self
+    ) -> None:
+        super().__init__(status_code=404)
+
+
+class IntellectoUnprocessableEntityError(IntellectoAPIStatusError):
+    def __init__(
+        self
+    ) -> None:
+        super().__init__(status_code=422)
+
+
+class IntellectoRateLimitError(IntellectoAPIStatusError):
+    def __init__(
+        self
+    ) -> None:
+        super().__init__(status_code=429)
+
+
+class IntellectoInternalServerError(IntellectoAPIStatusError):
+    def __init__(
+        self,
+        status_code: int
+    ) -> None:
+        super().__init__(status_code=status_code)
