e7d81e4bbdf3258067ddc5ea75881bd269b6067a
Kabir Asani <iamkabirasani@gmail.com>
2024-02-15 18:38:19 +0530

    Update qna.py

--- intellecto_python/tasks/nlp/types/qna.py
+++ intellecto_python/tasks/nlp/types/qna.py
@@ -1,5 +1,24 @@
+from dataclasses import dataclass
 from ...base import IntellectoBase
 
 
+@dataclass
 class NLPQnaModel(IntellectoBase):
-    pass
+    answer: str
+    score: float
+    start: int
+    stop: int
+
+    @staticmethod
+    def from_json(json: Any) -> 'NLPQnaModel':
+        answer = str(json.get('answer'))
+        score = str(json.get('score'))
+        start = int(obj.get('start'))
+        stop = str(obj.get('stop'))
+
+        return NLPQnaModel(
+            answer=answer,
+            score=score,
+            start=start,
+            stop=stop
+        )
