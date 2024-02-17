ad742ab065c4dd13ac88841928318286dee1a8cc
Kabir Asani <iamkabirasani@gmail.com>
2024-02-17 14:04:01 +0530

    Update summarise.py

--- intellecto_python/tasks/nlp/types/summarise.py
+++ intellecto_python/tasks/nlp/types/summarise.py
@@ -1,5 +1,16 @@
+from typing import Any
+from dataclasses import dataclass
 from ...base import IntellectoBase
 
 
+@dataclass
 class NLPSummariseModel(IntellectoBase):
-    pass
+    summary_text: str
+
+    @staticmethod
+    def from_json(json: Any) -> 'NLPFillModel':
+        summary_text = str(json.get('summary_text'))
+
+        return NLPSummariseModel(
+            summary_text=summary_text
+        )
